14d1cac832e481c5d62031b04493336340d99b19
T.DHINESH <145428367+Dinesh-TSD@users.noreply.github.com>
2025-05-27 16:15:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,7 @@ cloudinary.config({
 })
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+
 app.use(express.urlencoded({
     extended: true,
     limit: "20mb"
